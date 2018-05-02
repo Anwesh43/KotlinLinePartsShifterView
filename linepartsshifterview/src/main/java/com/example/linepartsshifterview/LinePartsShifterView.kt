1369e7630c1f9519b4e081b2c3293fedf27e6261
Anwesh43 <anweshthecool0@gmail.com>
2018-05-02 08:05:28 +0530

    adding LinePartsShifterView class

--- linepartsshifterview/src/main/java/com/example/linepartsshifterview/LinePartsShifterView.kt
+++ linepartsshifterview/src/main/java/com/example/linepartsshifterview/LinePartsShifterView.kt
@@ -8,3 +8,20 @@ import android.view.*
 import android.content.*
 import android.graphics.*
 
+class LinePartsShifterView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
