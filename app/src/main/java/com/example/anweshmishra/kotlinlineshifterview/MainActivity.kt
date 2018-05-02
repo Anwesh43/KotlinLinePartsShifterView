16387862583490cbf906023ec024e9917f4b941f
Anwesh43 <anweshthecool0@gmail.com>
2018-05-02 09:38:48 +0530

    addine LinePartsShifterView in Activity

--- app/src/main/java/com/example/anweshmishra/kotlinlineshifterview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinlineshifterview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.anweshmishra.kotlinlineshifterview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.linepartsshifterview.LinePartsShifterView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LinePartsShifterView.create(this)
     }
 }
